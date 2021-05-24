b1c82259f38bd29d856baacc2e48416a94bdb9bb
Exokem <ex010@live.com>
2021-05-24 16:26:41 -0400

    Added more utility macros - mainly instantiation and functional checking.

--- cut.h
+++ cut.h
@@ -1,6 +1,23 @@
 #ifndef _CUT_H
 #define _CUT_H
 
-#define inst( struct_id ) malloc ( sizeof ( struct struct_id ) );
+#include <stdlib.h>
+
+#define inst( struct_id ) malloc ( sizeof ( struct struct_id ) )
+#define iinst( instance, struct_id ) instance = ( ( void * ) 0 );\
+			instance = inst ( struct_id )
+#define setm( instance, field ) instance -> field = field
+#define setd( instance, field, value ) instance -> field = value
+#define nullck( ptr ) if ( ptr == NULL ) return
+#define nlcknl( ptr ) if ( ptr == NULL ) return NULL
+#define nlinst( instance, value ) instance = NULL;\
+			instance = value
+
+#define eqret( val1, val2, retval ) if ( val1 == val2 ) return retval
+#define eqact( val1, val2, act ) if ( val1 == val2 ) { act; }
+#define neqact( val1, val2, act ) if ( val1 != val2 ) { act; }
+#define sizeinst( inst, stid, amt ) inst = NULL;\
+			inst = malloc ( sizeof ( struct stid ) * amt )
+#define foreach( set, size ) for ( int ix = 0; ix < size; ix ++ )
 
 #endif
