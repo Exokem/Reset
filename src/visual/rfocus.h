10fd72bd7ab8f8e25fecb6c78688ac827270c53c
Exokem <ex010@live.com>
2021-06-15 18:40:56 -0400

    Expanded unfinished functionality of RFOCUS.

--- src/visual/rfocus.h
+++ src/visual/rfocus.h
@@ -3,18 +3,39 @@
 #define _RFOCUS_H
 
 #include "depn.h"
+#include "rrcon.h"
 #include "rvector.h"
+#include "rtilemap.h"
+#include "rlevelmap.h"
+
+typedef enum rfocus_mode_e
+{
+    F_FLAT_TILEMAP, F_LAYERED_TILEMAP
+} FocusMode;
 
 typedef struct rfocus_s
 {
-    // TODO: field for what it is looking at
-    // TODO: field for display mode ( tile-based, 3D, etc. )
+    FocusMode focus_mode;
 
-    VEC3D angles;
-    VEC3I offsets;
+    VEC3D angle;
+    VEC3I offset;
+
+    union content
+    {
+        RTILEMAP tilemap;
+        RLEVELMAP levelmap;
+    } content;
 
 } * RFOCUS;
 
-RFOCUS rfocus_inst ( VEC3D, VEC3I );
+RFOCUS rfocus_inst ( FocusMode );
+
+RFOCUS rfocus_vinst ( FocusMode, VEC3D, VEC3I );
+
+void rfocus_render_content ( RFOCUS, RRCON );
+
+void rfocus_translate ( RFOCUS, VEC3I );
+
+void rfocus_clr ( RFOCUS );
 
 #endif // _RFOCUS_H
